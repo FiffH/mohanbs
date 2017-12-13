535a24f0a4d0b1c6045555eb774aa674943de5df
FiffH <cs.harris@outlook.com>
2017-12-13 19:21:05 +0000

    fully responsive and functional services page

---
+++ mohanscript.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    
+    var visibleInfo = $();
+    
+    $('#services-info-container').hide();
+    $('.services-info-text').hide();
+    
+    $('.more-info').click(function(){
+        $('#services-info-container').show();
+        $('.services-info-text').hide();
+        $('#'+(event.target.id)+'-text').show();
+        $('#'+(event.target.id)+'-text').addClass("animated fadeInUp");
+    });
+
+    
+    
+});
