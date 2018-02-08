62d9a6a710b2e4cb1311d6210066a9681ba41e0a
FiffH <cs.harris@outlook.com>
2018-02-08 08:33:16 +0000

    Style Revamp
    Colours, style, and images mtach company branding better. cards have
    been slightly altered to be more aesthetically pleasing.

--- js/mohanscript.js
+++ js/mohanscript.js
@@ -2,11 +2,10 @@ $(document).ready(function() {
     $('#services-info-container').hide();
     $('.services-info-text').hide();
     
-    $('.more-info').click(function(event){
+    $('.services-card').click(function(event){
         $('#services-info-container').show();
         $('.services-info-text').hide();
         $('#'+(event.target.id)+'-text').show();
         $('#'+(event.target.id)+'-text').addClass("animated fadeInDown");
     });
 });
-  
